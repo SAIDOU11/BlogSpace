220c558f36a38042f856bc1659a5f5f9bb03e29f
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-13 06:30:03 +0200

    blogspace styling

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+fetch("https://apis.scrimba.com/jsonplaceholder/posts", { method: "GET" })
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
+    const postsArr = data.slice(0, 15);
+    document.getElementById("blog-list").innerHTML = postsArr
+      .map((post) => {
+        return `
+      <h3>${post.title}</h3>     
+      <p>${post.body}</p>
+      <hr />
+      `;
+      })
+      .join("");
   });
